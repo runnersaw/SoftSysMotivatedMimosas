474de571849bc5f69fc73c6a8818c556ac257acf
Sawyer Vaughan <sawyerv726@gmail.com>
2017-04-10 09:03:20 -0700

    Can now run files with arguments

--- gdb-lite/gdb-lite.c
+++ gdb-lite/gdb-lite.c
@@ -6,7 +6,53 @@
 
 #include "run.h"
 
-#define MAX_INPUT_LENGTH 100
+#define MAX_INPUT_LENGTH 1000
+
+char *get_input(char *input) {
+  printf("> ");
+  char *ret = fgets(input, MAX_INPUT_LENGTH, stdin);
+  if (ret == NULL) {
+    return ret;
+  }
+
+  // Clean newline
+  int len = strlen(input) -1;
+  if (input[len] == '\n') {
+    input[len] = '\0';
+  }
+
+  // Man page for fgets says it returns input string, so let's do that
+  return input;
+}
+
+char *rest_of_string_after_delimiter(char *input, char *delimiter) {
+  char *res = strstr(input, delimiter);
+  if (res == NULL) {
+    return "";
+  }
+  char *output = res + strlen(delimiter);
+  return output;
+}
+
+char *first_string_before_delimiter(char *input, char *delimiter) {
+  // Find first instance of delimiter
+  char *res = strstr(input, delimiter);
+
+  int len;
+  // If not found, copy whole string. Else, the length we need to copy is the difference between the two.
+  if (res == NULL) {
+    len = strlen(input);
+  } else {
+    len = (res - input) / sizeof(char);
+  }
+  char *output = malloc((len + 1) * sizeof(char));
+  int i;
+  for (i = 0; i < len; i++) {
+    output[i] = input[i];
+  }
+  output[len] = '\0';
+  return output;
+}
 
 int main(int argc, char *argv[]) {
   char *input = malloc((MAX_INPUT_LENGTH + 1) * sizeof(char));
@@ -19,11 +65,18 @@ int main(int argc, char *argv[]) {
 
   char *fname = argv[1];
 
-  while (fgets(input, MAX_INPUT_LENGTH, stdin) != NULL) {
+  while (get_input(input) != NULL) {
+    // Split on whitespace
+    char *command = first_string_before_delimiter(input, " ");
+
     // Interpret command here
-    if (strcmp(input, "run") == 0) {
-      int code = run(fname);
-      printf("%s exited with code %d\n", fname, code);
+    if (strcmp(command, "run") == 0) {
+      char *args = rest_of_string_after_delimiter(input, " ");
+
+      int code = run(fname, args);
+      printf("\n%s exited with code %d\n", fname, code);
+    } else if (strcmp(command, "quit") == 0) {
+      return 0;
     } else {
       printf("Command not handled: %s\n", input);
     }
--- gdb-lite/include/run.h
+++ gdb-lite/include/run.h
@@ -1,4 +1,4 @@
 
-int run(char *fname);
+int run(char *fname, char *args);
 
 
