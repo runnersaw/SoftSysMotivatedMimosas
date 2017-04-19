3ece2ef849086e3e2d63a01b19d740aeb634e385
Chris Wallace <christopher.wallace@students.olin.edu>
2017-04-19 19:34:20 -0400

    WIP on running breakpoints

--- gdb-lite/gdb-lite.c
+++ gdb-lite/gdb-lite.c
@@ -5,10 +5,12 @@
 #include <unistd.h>
 
 #include "run.h"
+#include "breakpoint.h"
 
 #define MAX_INPUT_LENGTH 100
 
 int main(int argc, char *argv[]) {
+  int pid;
   char *input = malloc((MAX_INPUT_LENGTH + 1) * sizeof(char));
   input[MAX_INPUT_LENGTH] = '\0';
 
@@ -22,8 +24,19 @@ int main(int argc, char *argv[]) {
   while (fgets(input, MAX_INPUT_LENGTH, stdin) != NULL) {
     // Interpret command here
     if (strcmp(input, "run") == 0) {
-      int code = run(fname);
-      printf("%s exited with code %d\n", fname, code);
+      pid = fork();
+
+      if (pid == 0) {
+        //If it is the child then we wanna run this
+        int code = run(fname);
+        printf("%s exited with code %d\n", fname, code);
+      } else {
+        insertBreakpoint(pid);
+        //We are the parent we want to attach a process to this PID
+        
+      }
+      
+      
     } else {
       printf("Command not handled: %s\n", input);
     }
---
+++ gdb-lite/include/breakpoint.h
@@ -0,0 +1,1 @@
+int insertBreakpoint(int pid);
