9f6fb3f0ca598d5802217ebf9a3afbf76d73b283
osboxes.org <christopher.wallace@students.olin.edu>
2017-04-23 10:05:15 +0100

    Added breakpoint code

--- gdb-lite/gdb-lite.c
+++ gdb-lite/gdb-lite.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/reg.h>
+#include <sys/ptrace.h>
 
 #include "run.h"
 #include "breakpoint.h"
@@ -23,21 +25,27 @@ int main(int argc, char *argv[]) {
 
   while (fgets(input, MAX_INPUT_LENGTH, stdin) != NULL) {
     // Interpret command here
-    if (strcmp(input, "run") == 0) {
-      pid = fork();
+    if (strcmp(input, "run\n") == 0) {
 
-      if (pid == 0) {
         //If it is the child then we wanna run this
         int code = run(fname);
+        printf("%s exited with code %d\n", fname, code);
+      
+      
+    } else if (strcmp(input, "break\n") == 0) {
+      pid = fork();
+      if (pid == 0) {
+        //If it is the child then we wanna run this
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        int code = execl(fname, fname, 0);
         printf("%s exited with code %d\n", fname, code);
       } else {
         insertBreakpoint(pid);
         //We are the parent we want to attach a process to this PID
         
       }
-      
-      
     } else {
+      printf("Value is %i\n",strcmp(input, "run\n"));
       printf("Command not handled: %s\n", input);
     }
   }
--- gdb-lite/src/breakpoint.c
+++ gdb-lite/src/breakpoint.c
@@ -1,48 +1,45 @@
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include <linux/user.h>   /* For user_regs_struct
-                             etc. */
-int main(int argc, char *argv[])
-{   pid_t traced_process;
-    struct user_regs_struct regs;
-    long ins;
-    if(argc != 2) {
-        printf("Usage: %s <pid to be traced>\n",
-               argv[0], argv[1]);
-        exit(1);
-    }
-    traced_process = atoi(argv[1]);
-    ptrace(PTRACE_ATTACH, traced_process,
-           NULL, NULL);
-    wait(NULL);
-    ptrace(PTRACE_GETREGS, traced_process,
-           NULL, &regs);
-    ins = ptrace(PTRACE_PEEKTEXT, traced_process,
-                 regs.eip, NULL);
-    printf("EIP: %lx Instruction executed: %lx\n",
-           regs.eip, ins);
-    ptrace(PTRACE_DETACH, traced_process,
-           NULL, NULL);
-    return 0;
-}
+#include <sys/user.h>
+#include <sys/reg.h>
+#include <unistd.h> 
 
+#include "breakpoint.h"
 int insertBreakpoint(int pid) {
   pid_t traced_process;
+   int wait_status;
+
   struct user_regs_struct regs;
   long ins;
+
+  //run objdump -d on the executable you want to find
+  //Location of insturction to stop on
+  unsigned breakAddress = 0x4005a2;
   traced_process = pid;
-  ptrace(PTRACE_ATTACH, traced_process,
-         NULL, NULL);
   wait(NULL);
+  //GEt the contents of registers
   ptrace(PTRACE_GETREGS, traced_process,
          NULL, &regs);
-  ins = ptrace(PTRACE_PEEKTEXT, traced_process,
-               regs.eip, NULL);
-  printf("EIP: %lx Instruction executed: %lx\n",
-         regs.eip, ins);
-  ptrace(PTRACE_DETACH, traced_process,
-         NULL, NULL);
+  //find the instruction at the memory address
+  ins = ptrace(PTRACE_PEEKTEXT, traced_process, 
+    (void*)breakAddress, 0);
+  //IDK why this is really being done, but it codes for the trap instruction
+  unsigned data_with_trap = (ins & 0xffffff00) | 0xCC;
+  //Write trap instruction
+  ptrace(PTRACE_POKETEXT, traced_process, breakAddress, data_with_trap);
+  printf ("Data now %08x \n", ptrace(PTRACE_PEEKTEXT, traced_process, breakAddress, 0));
+  //Keep it going
+  ptrace(PTRACE_CONT, traced_process, 0,0 );
+  //wait(&wait_status);
+  // if (WIFSTOPPED(wait_status)) {
+  //   printf("Child got signal %s\n", strsignal(WSTOPSIG(wait_status)));
+
+  // }
+  //Check registers again
+  ptrace(PTRACE_GETREGS, traced_process,
+         NULL, &regs);
+  //print location of instruction pointer, should be +1 of breakADdress
+  printf("Child stopped at %08x\n", regs.rip);
   return 0;
 }
