f7eb3d0cc65514342aa454ca71b80ed519646dd9
chongswee <chongswee.goh@stuents.olin.edu>
2017-05-01 18:52:50 -0400

    stack dump

---
+++ gdb-lite/include/dump.h
@@ -0,0 +1,6 @@
+#ifndef DUMP_H_   /* Include guard */
+#define DUMP_H_
+
+void printStack(Breakpoint* breakpoint);
+
+#endif // FOO_H_
---
+++ gdb-lite/src/dump.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#include <sys/wait.h>
+#include <sys/user.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "breakpoint.h"
+#include "dump.h"
+
+//function for printing of registers
+#define printreg(REGNAME) printf("%3s: 0x%016llx\n", #REGNAME, regs.REGNAME)
+
+struct user_regs_struct regs;
+
+void printStack(Breakpoint* breakpoint){
+  unsigned long addr = breakpoint->address;
+  int child_pid = breakpoint->pid;
+  ptrace(PTRACE_GETREGS, child_pid, 0, &regs);
+
+  printreg(rsp);
+  printreg(rbp);
+
+  if (regs.rbp <= regs.rsp){
+    fprintf(stderr,"Error in dumping stack, invalid breakpoint?\n");
+    return;
+  }
+
+  int i = 0;
+  void* rsp_addr = (void*)((unsigned long int)(regs.rsp + i));
+
+  while((unsigned long int)rsp_addr!= regs.rbp){
+    long rsp_val = ptrace(PTRACE_PEEKDATA, child_pid, rsp_addr, NULL);
+
+    if(rsp_val != -1) {
+    printf("(Address:0x%016lx), Value(32 bits): % 16d or 0x%08x\n", (unsigned long int) rsp_addr, rsp_val, rsp_val);
+    } else {
+      printf("Error in reading register : 0x%016lx",(unsigned long int) rsp_addr);
+    }
+    i += 4; // I think each address stores a byte? , printing each 32 bits
+    rsp_addr = (void*)((unsigned long int)(regs.rsp + i));
+  }
+}
